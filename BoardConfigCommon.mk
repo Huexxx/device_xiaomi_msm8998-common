8231f99aeea5ff6973b7292954852acf2d183dec
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    msm8998-common: Clean up kernel cmdline
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,7 @@ TARGET_KERNEL_CLANG_VERSION := cosmic
 TARGET_KERNEL_VERSION := 4.4
 
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
-BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1da4000.ufshc
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
