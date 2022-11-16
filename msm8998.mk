71a9789ccabff2a759be0a73d332beb9ba51c882
Huexxx <huexxx@gmail.com>
2022-11-16 13:45:30 +0100

    msm8998-common: Run ipa startup asynchronously

--- msm8998.mk
+++ msm8998.mk
@@ -422,6 +422,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.panel_info.sh \
     init.qcom.early_boot.sh \
+    init.qcom.ipastart.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh
 
