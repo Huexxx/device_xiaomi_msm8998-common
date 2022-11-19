184d590183b37590c3880f9fec8d2f6e00661fc2
Huexxx <huexxx@gmail.com>
2022-11-19 11:47:01 +0100

    msm8998-common: audio: Update android.hardware.audio to version 7.1
    - Also change android.hardware.audio@2.0-service to android.hardware.service

--- msm8998.mk
+++ msm8998.mk
@@ -124,8 +124,8 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl
 
