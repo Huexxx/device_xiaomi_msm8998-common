ef1f49dfb51830ea96f91ecc66560df8c942192e
Huexxx <huexxx@gmail.com>
2022-11-19 11:49:07 +0100

    msm8998-common: audio: Drop android.hardware.soundtrigger HAL

--- msm8998.mk
+++ msm8998.mk
@@ -121,8 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
