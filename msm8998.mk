273bd9f5e4bfe966e08edb28f936a192f9c8ebd8
Huexxx <huexxx@gmail.com>
2022-12-07 16:58:56 +0100

    Revert "msm8998-common: bluetooth: Build audio.hearing_aid.default"
    This reverts commit f47343f00e06b10fba01ba17a56135e0f65311d0.

--- msm8998.mk
+++ msm8998.mk
@@ -132,8 +132,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    android.hardware.bluetooth.a2dp@1.0-impl.mock \
-    audio.hearing_aid.default
+    android.hardware.bluetooth.a2dp@1.0-impl.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
