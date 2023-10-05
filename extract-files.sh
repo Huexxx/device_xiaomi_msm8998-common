2d6489885fc9232d5aad77909e5ff190199667b4
me-cafebabe <me.cafebabe@gmail.com>
2023-10-05 22:36:24 +0800

    msm8998-common: Load full protobuf library instead of lite
    * There's just lite variant in prebuilts/misc/protobuf_vendorcompat
    
    Change-Id: Idac18d067721d03a7a188e436d9a99504731e5e9
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- extract-files.sh
+++ extract-files.sh
@@ -125,6 +125,9 @@ function blob_fixup() {
         "${PATCHELF}" --remove-needed "libgui.so" "${2}"
         ;;
 
+    vendor/lib64/libwvhidl.so)
+        "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+        ;;
     esac
 }
 
