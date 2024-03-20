bce52653daf2ecc2e226ea6a5e0e44f181a8dd59
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    msm8998-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- msm8998.mk
+++ msm8998.mk
@@ -18,9 +18,6 @@ NEED_AIDL_NDK_PLATFORM_BACKEND  := true
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Disable APEX compression
 # Keep this after including updatable_apex.mk
 PRODUCT_COMPRESSED_APEX := false
