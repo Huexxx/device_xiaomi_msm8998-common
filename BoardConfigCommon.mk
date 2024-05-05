412be244930a947116fcf57157db00ee8f23218d
Georg Veichtlbauer <georg@vware.at>
2024-05-05 09:21:07 +0700

    msm8998-common: powerhint: Let libperfmgr handles DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,7 +135,6 @@ TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
