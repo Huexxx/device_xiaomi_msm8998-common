1e47b5c568c80a589c46b2587ce6270a45d95838
Huexxx <huexxx@gmail.com>
2024-08-26 16:17:27 +0200

    msm8998-common: Complete QSEECOM blobs from Fairphone 3
    * From Fairphone/FP3/FP3:13/6.A.028.1/gms-fb581341:user/release-keys

--- msm8998.mk
+++ msm8998.mk
@@ -270,6 +270,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     libhidlmemory.vendor:64 \
